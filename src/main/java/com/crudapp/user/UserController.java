a4cfddba554101e41f249042852c7092a525c8ab
Mohammed Abdul-Aziz <48477328+tajutechgh@users.noreply.github.com>
2025-09-25 00:35:13 +0200

    added crud ui fragmentations

--- src/main/java/com/crudapp/user/UserController.java
+++ src/main/java/com/crudapp/user/UserController.java
@@ -1,13 +1,16 @@
 package com.crudapp.user;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class UserController {
 
     @GetMapping("/")
-    public String viewIndexPage(){
+    public String viewIndexPage(Model model){
+
+        model.addAttribute("pageTitle", "Manage Users");
 
         return "/index";
     }
