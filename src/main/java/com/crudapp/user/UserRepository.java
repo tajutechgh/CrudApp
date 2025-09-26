9bfd59f719244d5aef0034d2085b0f55a80a78c4
Mohammed Abdul-Aziz <48477328+tajutechgh@users.noreply.github.com>
2025-09-26 20:42:45 +0200

    removed unused imports

--- src/main/java/com/crudapp/user/UserRepository.java
+++ src/main/java/com/crudapp/user/UserRepository.java
@@ -1,6 +1,5 @@
 package com.crudapp.user;
 
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
